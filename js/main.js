cd601d1bdc610938b99c01f5e0213d13b5a1a69e
Fenna03 <fenna.manders@sintlucasedu.nl>
2024-02-09 12:52:03 +0100

    idk man i'm missing some shit

--- js/main.js
+++ js/main.js
@@ -8,15 +8,44 @@ window.onload = (event) => {
     const hero = document.getElementById("Hero");
     const offsetcharacter = 16;
 
+    const tree1 = document.getElementById("squareTree");
+    const door = document.getElementById("door");
+    const key = document.getElementById("key");
+
     gameWindow.onclick = function (e) {
         var rect = gameWindow.getBoundingClientRect();
         var x = e.clientX - rect.left;
         var y = e.clientY - rect.top;
 
+        //No anim first tap
+        //char out of bounds
+        //TODO: use fancy way with style to get the ofset right
         hero.style.left = x - offsetcharacter + 'px';
         hero.style.top = y - offsetcharacter + 'px';
 
+        switch (e.target.id) {
+            case "squareTree":
+                tree1.style.opacity = 0.5;
+                break;
+            case "door":
+                door.style.opacity = 0.5;
+                break;
+            case "key":
+                console.log("You\'ve found a key!");
+                document.getElementById("key").remove;
+                break;
+        }
+        if (e.target.id == "squareTree") {
+            tree1.style.opacity = 0.5;
+        } else {
+            tree1.style.opacity = 1;
+        }
 
-        //console.log(x + ", " + y);
+        if (e.target.id == "door") {
+            door.style.opacity = 0.5;
+        } else {
+            door.style.opacity = 1;
+        }
+
     }
 };
