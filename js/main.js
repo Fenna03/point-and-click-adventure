846126f59715c865c2bd5b0ba78347bad9f78aa0
Fenna03 <fenna.manders@sintlucasedu.nl>
2024-02-27 12:54:36 +0100

    mc not moving when talking
    audio for talking, cannot get items when talking

--- js/main.js
+++ js/main.js
@@ -17,6 +17,13 @@ window.onload = () => {
         const hero = document.getElementById("Hero");
         const offsetcharacter = 16;
 
+        //avatar
+        const counterAvatar = document.getElementById("CounterAvatar");
+
+        //audio for dialog
+        const heroAudio = document.getElementById("heroAudio");
+        const counterAudio = document.getElementById("counterAudio");
+
         //speech Bubbles
         const heroSpeech = document.getElementById("heroSpeech");
         const counterSpeech = document.getElementById("counterSpeech");
@@ -29,55 +36,67 @@ window.onload = () => {
         let heroX = 0;
         let heroY = 0;
 
+        //check if dialog is dialogging
+
         // Handle click events
         gameWindow.onclick = function (e) {
             var rect = gameWindow.getBoundingClientRect();
             var x = e.clientX - rect.left;
             var y = e.clientY - rect.top;
 
-            if (e.target.id !== "HeroImage") {
-                // Set the position of the hero
-                heroX = x - offsetcharacter;
-                heroY = y - offsetcharacter;
 
-                // Move the hero
-                hero.style.left = heroX + 'px';
-                hero.style.top = heroY + 'px';
-            }
-            //making things happen when you touch one of the objects
-            switch (e.target.id) {
-                case "key":
-                    showMessage(heroSpeech, "I found a key!")
-                    console.log("pick up key")
-                    document.getElementById("key").remove();
-                    changeInventory('key', "add");
-                    break;
-                case "well":
-                    if (gameState.coinPickedUp == false) {
-                        showMessage(heroSpeech, "I found a coin!"); 8
-                        console.log("you found a coin!");
-                        changeInventory("coin", "add");
-                        gameState.coinPickedUp = true;
-                    } else {
-                        showMessage(heroSpeech, "seems like I got all the coins from here");
-                    }
-                    break;
-                case "doorWizardHut":
-                    if (checkItem("key")) {
-                        showMessage(heroSpeech, "I opened the cave!");
-                        // changeInventory('key', 'remove');
-                    } else if (checkItem("coin")) {
-                        showMessage(heroSpeech, "I press the coin against the stone and it disappears. damn")
-                        changeInventory('coin', 'remove');
-                    } else {
-                        showMessage(counterSpeech, "The cave seems to be locked");
-                    }
-                    break;
-                case "statue":
-                    showMessage(counterSpeech, "Hello you want to get in the cave? look in the boxes");
-                    break;
-                default:
-                    break;
+
+            if (counterSpeech.style.opacity == 0 && heroSpeech.style.opacity == 0) {
+
+                if (e.target.id !== "HeroImage") {
+                    // Set the position of the hero
+                    heroX = x - offsetcharacter;
+                    heroY = y - offsetcharacter;
+
+                    // Move the hero
+                    hero.style.left = heroX + 'px';
+                    hero.style.top = heroY + 'px';
+                }
+
+                //making things happen when you touch one of the objects
+                switch (e.target.id) {
+                    case "key":
+                        showMessage(heroSpeech, "I found a key!", heroAudio);
+                        document.getElementById("key").remove();
+                        changeInventory('key', "add");
+                        break;
+                    case "well":
+                        if (gameState.coinPickedUp == false) {
+                            showMessage(heroSpeech, "I found a coin!", heroAudio);
+                            changeInventory("coin", "add");
+                            gameState.coinPickedUp = true;
+                        } else {
+                            showMessage(heroSpeech, "seems like I got all the coins from here", heroAudio);
+                        }
+                        break;
+                    case "doorWizardHut":
+                        if (checkItem("key")) {
+                            showMessage(heroSpeech, "I opened the cave!", heroAudio);
+                            // changeInventory('key', 'remove');
+                        } else if (checkItem("coin")) {
+                            showMessage(heroSpeech, "I press the coin against the stone and it disappears. damn", heroAudio);
+                            changeInventory('coin', 'remove');
+                        } else {
+                            showMessage(heroSpeech, "The cave seems to be locked", heroAudio);
+                        }
+                        break;
+                    case "statue":
+                        showMessage(heroSpeech, "huh, a barrel.", heroAudio);
+                        setTimeout(function () { counterAvatar.style.opacity = 1; }, 4 * sec);
+                        setTimeout(showMessage, 4 * sec, counterSpeech, "I'm not a barrel! I'm hiding IN A barrel!", counterAudio);
+                        setTimeout(showMessage, 8 * sec, heroSpeech, "From the thing in the cave? I'll kill it.", heroAudio);
+                        setTimeout(showMessage, 12 * sec, counterSpeech, "check the boxes for the key to get in the cave! just save me!!!", counterAudio);
+                        setTimeout(showMessage, 16 * sec, heroSpeech, "No questions asked huh... damnn", heroAudio);
+                        setTimeout(function () { counterAvatar.style.opacity = 0; }, 18 * sec);
+                        break;
+                    default:
+                        break;
+                }
             }
 
             /**
@@ -127,26 +146,29 @@ window.onload = () => {
             }
         }
         /** 
-         * it will show dialog
+         * it will show dialog and trigger sound
          * @param {getElementById} targetBubble 
          * @param {string} message 
+         * @param {getElementById} targetSound
          */
-        function showMessage(targetBubble, message) {
+        function showMessage(targetBubble, message, targetSound) {
+            targetSound.currentTime = 0;
+            targetSound.play();
             targetBubble.innerText = message;
             targetBubble.style.opacity = 1;
-            setTimeout(hideMessage, 4 * sec, targetBubble);
+            setTimeout(hideMessage, 4 * sec, targetBubble, targetSound);
         }
+
         /**
-         * 
-         * @param {*} targetBubble 
+         * hides dialog and pauses sound.
+         * @param {getElementById} targetBubble 
+         * @param {getElementById} targetSound 
          */
-        function hideMessage(targetBubble) {
+        function hideMessage(targetBubble, targetSound) {
+            targetSound.pause();
             targetBubble.innerText = "... ";
             targetBubble.style.opacity = 0;
         }
-
-        showMessage(heroSpeech, "ello!");
-        showMessage(counterSpeech, "wassup");
     }
     runGame();
 };
